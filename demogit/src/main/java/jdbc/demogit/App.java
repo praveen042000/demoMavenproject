12e5798a846577e8bc8553cc2b5680002926f2b4
praveen042000 <146086471+praveen042000@users.noreply.github.com>
2023-09-26 18:31:39 +0530

    Maven java project

---
+++ demogit/src/main/java/jdbc/demogit/App.java
@@ -0,0 +1,13 @@
+package jdbc.demogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
